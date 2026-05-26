40b42eb3baecba8501b75eda2863327f25168011
Nisalmon <nisalmon@student.42lehavre.fr>
2026-05-26 16:19:12 +0200

    Added docstrings

--- utils/utils_print.py
+++ utils/utils_print.py
@@ -5,6 +5,16 @@ from typing import Tuple, Dict, Any
 def get_leaderboard(screen: Dict[str, Any],
                     scorers: Dict[str, int],
                     size: Tuple[int, int]) -> None:
+    '''
+    To print the leaderboard in score section.
+
+    :params:
+        - screen: Dict: The screen configuration dict.
+
+        - scorers: Dict: The dict containing the highest scorers.
+
+        - size: Tuple: The size of the screen.
+    '''
     loc = (size[0] // 2, size[1] - 3*size[1]//4)
     txt = "Highscores:"
     screen['font'].render_to(screen['screen'],
@@ -23,6 +33,16 @@ def get_leaderboard(screen: Dict[str, Any],
 def print_score(score: int,
                 screen: Dict[str, Any],
                 size: Tuple[int, int]) -> None:
+    '''
+    To print the current score during game.
+
+    :params:
+        - score: int: Current score of player
+
+        - screen: Dict: The screen configuration dict.
+
+        - size: Tuple: The size of the screen.
+    '''
     loc1 = (size[0] + 20, 12)
     loc2 = (size[0] + 20, 36)
     screen['font'].render_to(screen['screen'], loc1,
@@ -35,6 +55,18 @@ def print_life(sprite: pg.Surface,
                lives: int,
                screen: Dict[str, Any],
                size: Tuple[int, int]) -> None:
+    '''
+    To print the amount of lives the player have.
+
+    :params:
+        - sprite: Surface: The sprite of lives
+
+        - lives: int: The amount of lives of the player
+
+        - screen: Dict: The screen configuration dict.
+
+        - size: Tuple: The size of the screen.
+    '''
     loc1 = (size[0] + 20, 96)
     loc2 = (size[0] + 20, 120)
     screen['font'].render_to(screen['screen'], loc1,
@@ -48,6 +80,16 @@ def print_life(sprite: pg.Surface,
 def print_timer(time: int,
                 screen: Dict[str, Any],
                 size: Tuple[int, int]) -> None:
+    '''
+    To print the remaining time on screen.
+
+    :params:
+        - screen: Dict: The screen configuration dict.
+
+        - time: int: time left to finish the level.
+
+        - size: Tuple: The size of the screen
+    '''
     loc1 = (size[0] + 20, 180)
     loc2 = (size[0] + 20, 204)
     screen['font'].render_to(screen['screen'], loc1,
@@ -60,6 +102,18 @@ def print_level(current: int,
                 max_lvl: int,
                 screen: Dict[str, Any],
                 size: Tuple[int, int]) -> None:
+    '''
+    To print current level the player is on out of max_lvl
+
+    :params:
+        - current: int: The level the player is on
+
+        - max_lvl: int: The number of levels in game
+
+        - screen: Dict: The screen configuration dict.
+
+        - size: Tuple: The size of the screen.
+    '''
     loc1 = (size[0] + 20, 260)
     screen['font'].render_to(screen['screen'], loc1,
                              f"Level {current}/{max_lvl}",
@@ -74,6 +128,26 @@ def print_all(screen: Dict[str, Any],
               time: int,
               level: int,
               nb_level: int) -> None:
+    '''
+    To print score, life, timer and level.
+
+    :params:
+        - screen: Dict: The screen configuration dict.
+
+        - size: Tuple: The size of the screen.
+
+        - score: int: Current score of player
+
+        - sprite: Surface: The sprite of lives
+
+        - lives: int: The amount of lives of the player
+
+        - time: int: time left to finish the level.
+
+        - level: int: The level the player is on
+
+        - nb_level: int: The number of levels in game
+    '''
     print_score(score, screen, size)
     print_life(sprite, lives, screen, size)
     print_timer(time, screen, size)
@@ -83,6 +157,16 @@ def print_all(screen: Dict[str, Any],
 def print_countdown(screen: Dict[str, Any],
                     value: int,
                     size: Tuple[int, int]) -> None:
+    '''
+    To print countdown upon respawning.
+
+    :params:
+        - screen: Dict: The screen configuration dict.
+
+        - value: int: The time left before being able to play
+
+        - size: Tuple: The size of the screen
+    '''
     loc = ((size[0] - 300) // 2 - 10, size[1] // 2 - 72)
     screen['font'].render_to(screen['screen'], loc,
                              f"{value}", (255, 255, 255))
@@ -90,6 +174,14 @@ def print_countdown(screen: Dict[str, Any],
 
 def print_paused(screen: Dict[str, Any],
                  size: Tuple[int, int]) -> None:
+    '''
+    To print paused on screen
+
+    :params:
+        - screen: Dict: The screen configuration dict.
+
+        - size: Tuple: The size of the screen
+    '''
     loc = (size[0]//2, size[1]//2)
     txt = [
         "Paused",
@@ -104,6 +196,12 @@ def print_paused(screen: Dict[str, Any],
 
 
 def print_guide(screen: Dict[str, Any]) -> None:
+    '''
+    To print instructions of how to play on screen
+
+    :params:
+        - screen: Dict: The screen configuration dict.
+    '''
     instr = [
         "Move using arrow keys.",
         "In game, press ESC to pause the game.",
@@ -123,6 +221,16 @@ def print_guide(screen: Dict[str, Any]) -> None:
 def end_game_print(screen: Dict[str, Any],
                    score: int,
                    size: Tuple[int, int]) -> None:
+    '''
+    To print the final score on screen
+
+    :params:
+        - screen: Dict: The screen configuration dict.
+
+        - score: int: Final score of the game.
+
+        - size: Tuple: The size of the screen.
+    '''
     loc1 = (size[0] // 2 - 116, size[1] // 2 - 48)
     loc2 = (size[0] // 2, size[1] // 2)
     screen['font'].render_to(screen['screen'], loc1,
@@ -133,6 +241,14 @@ def end_game_print(screen: Dict[str, Any],
 
 def congrats_print(screen: Dict[str, Any],
                    size: Tuple[int, int]) -> None:
+    '''
+    To print Congratulation on screen
+
+    :params:
+        - screen: Dict: The screen configuration dict.
+
+        - size: Tuple: The size of the screen.
+    '''
     loc = (size[0] // 2, size[1] // 2 - 148)
     txt = "Congratulation"
     screen['font'].render_to(screen['screen'], (loc[0] - (len(txt) // 2) * 24,
@@ -142,6 +258,14 @@ def congrats_print(screen: Dict[str, Any],
 
 def game_over_print(screen: Dict[str, Any],
                     size: Tuple[int, int]) -> None:
+    '''
+    To print GameOver on screen
+
+    :params:
+        - screen: Dict: The screen configuration dict.
+
+        - size: Tuple: The size of the screen.
+    '''
     loc = (size[0] // 2, size[1] // 2 - 148)
     txt = "Game Over..."
     screen['font'].render_to(screen['screen'], (loc[0] - (len(txt) // 2) * 24,
@@ -152,6 +276,16 @@ def game_over_print(screen: Dict[str, Any],
 def get_username(screen: Dict[str, Any],
                  username: str,
                  size: Tuple[int, int]) -> None:
+    '''
+    To print the input of the player for username
+
+    :params:
+        - screen: Dict: The screen configuration dict.
+
+        - size: Tuple: The size of the screen.
+
+        - username: str: The username that will be stored in scorers.
+    '''
     loc1 = (size[0] // 2 - 116, size[1] // 2 + 48)
     loc2 = (size[0] // 2, size[1] // 2 + 80)
     screen['font'].render_to(screen['screen'], loc1,
@@ -162,6 +296,14 @@ def get_username(screen: Dict[str, Any],
 
 def get_cheats(screen: Dict[str, Any],
                cheats: Dict[str, bool]) -> None:
+    '''
+    To print all available cheats in cheat menu
+
+    :params:
+        - screen: Dict: The screen configuration dict.
+
+        - cheats: Dict: All available cheats
+    '''
     cnt = 0
     for name, value in cheats.items():
         color = (255, 0, 0) if not value else (0, 255, 0)
